506854e9450adb125e419c440c56098eeced624e
zola07 <lazarmilivojevic07@gmail.com>
2018-12-04 15:52:24 +0100

    Create first html page

--- django19/django19/settings.py
+++ django19/django19/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = "/Users/User.PC/Desktop/django/django19/django19"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
